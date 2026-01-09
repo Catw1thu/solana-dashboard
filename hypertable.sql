16344865a67e439cdbbf95a28e7e2a5adaf78b5b
Catw1thu <486951583@qq.com>
2026-01-09 20:28:14 +0800

    chore: remove unused hypertable.sql

--- hypertable.sql
+++
@@ -1,8 +0,0 @@
--- 安装超表插件
-CREATE EXTENSION IF NOT EXISTS timescaledb;
--- 查看超表
-SELECT * FROM pg_extension WHERE extname = 'timescaledb';
--- 开启超表
-SELECT create_hypertable('"Trade"', 'time');
--- 查看所有超表
-SELECT * FROM timescaledb_information.hypertables;
