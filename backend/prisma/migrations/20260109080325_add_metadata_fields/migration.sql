bb6eb47b64172b28acbb674cc72e05432f261e3a
catw1thu <486951583@qq.com>
2026-04-06 16:21:51 +0800

    refator

--- backend/prisma/migrations/20260109080325_add_metadata_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Pool" ADD COLUMN     "image" TEXT,
-ADD COLUMN     "name" TEXT,
-ADD COLUMN     "symbol" TEXT;
--- backend/src/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventsGateway } from './events.gateway';
-import { BatcherService } from './batcher.service';
-
-@Module({
-  providers: [EventsGateway, BatcherService],
-  exports: [EventsGateway, BatcherService],
-})
-export class EventsModule {}
