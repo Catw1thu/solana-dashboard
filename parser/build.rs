7e16f33b6d447608e4f51b34b52ab735dd46fa8b
catw1thu <486951583@qq.com>
2026-04-11 21:25:09 +0800

    chore: finalize monorepo migration cleanup

--- parser/build.rs
+++ parser/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let proto_file = "proto/service_event.proto";
+    let proto_file = "../proto/service_event.proto";
     println!("cargo:rerun-if-changed={proto_file}");
 
     let protoc_path = protoc_bin_vendored::protoc_bin_path().expect("find vendored protoc");
@@ -8,6 +8,6 @@ fn main() {
     config.protoc_executable(protoc_path);
 
     config
-        .compile_protos(&[proto_file], &["proto"])
+        .compile_protos(&[proto_file], &["../proto"])
         .expect("compile service event protobuf");
 }
